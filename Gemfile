14e0c4993cff3855ca0bdc0e73bd7eb2aff4d0a5
Willie Lima <w@willielima.me>
2024-02-03 17:15:42 +0000

    Move project to version 2

--- Gemfile
+++
@@ -1,9 +0,0 @@
-source "https://rubygems.org"
-
-
-gem 'sinatra'
-gem 'hyperkit'
-gem 'faraday', '0.17.3'
-gem 'require_all'
-gem 'shotgun'
-gem 'redis'
--- config/environment.rb
+++
@@ -1,9 +0,0 @@
-ENV['SINATRA_ENV'] ||= "development"
-
-require 'bundler/setup'
-Bundler.require(:default, ENV['SINATRA_ENV'])
-
-
-require_relative 'boot'
-
-require_all 'app'
