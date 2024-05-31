33955ee65d6a319ede9d4e6ca728faaefb5eeeb9
Willie Lima <willie.lima@wiline.com>
2024-05-31 10:09:15 +0100

    Add dynamic backend API definition

--- src/api.jsx
+++ src/api.jsx
@@ -1,4 +1,15 @@
-export const API_URL = import.meta.env.VITE_API_URL;
+
+const getApiUrl = () => {
+    const config_api_url = import.meta.env.VITE_API_URL;
+
+    if (!config_api_url) {
+        return `${window.location}api/v1/containers`;
+    }
+
+    return config_api_url;
+}
+
+const API_URL = getApiUrl();
 
 export const fetchContainers = async () => {
     const response = await fetch(API_URL);
